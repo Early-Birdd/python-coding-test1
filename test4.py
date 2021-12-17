461d75a592d17babf2a7898f8d7d380947feb014
EarlyBird <goqhwk1100@nate.com>
2021-12-17 18:30:19 +0900

    commit

--- test4.py
+++ test4.py
@@ -15,3 +15,21 @@
 #
 # print(count)
 
+n, k = map(int, input().split())
+
+count = 0
+target = 0
+
+while True:
+    target = (n // k) * k
+    count += (n - target)
+    n = target
+
+    if n < k:
+        break
+
+    count += 1
+    n //= k
+
+count += (n - 1)
+print(count)
